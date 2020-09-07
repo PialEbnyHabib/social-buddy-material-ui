7a8659e7cf19935b476abac3d1d1730ae8d7110e
pial ebny habib <khanmd.pialebnyhabib@gmail.com>
2020-09-07 20:17:46 +0600

    first commit

---
+++ src/commentPic/commentPic.js
@@ -0,0 +1,8 @@
+var CommentPic = [ 
+    { 
+        img : "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",  
+    },
+
+]
+
+export default CommentPic;
